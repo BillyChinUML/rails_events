a5bf78c76d688b53897cdcdc2cc0b19992072596
Billy <Billy_Chin@student.uml.edu>
2022-07-26 13:54:48 -0400

    Signed in users can now post events. Modified home page to make it more visually appealing.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,36 @@
 class EventsController < ApplicationController
 
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @events = Event.all
   end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @user = current_user
+    @event = @user.created_events.build(event_params)
+    @event.user_id = @user.id
+    @event.creator_id = @event.user_id
+
+    if @event.save
+      redirect_to user_path(@user)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  
+  def event_params
+    params.require(:event).permit(:name, :date, :location, :description)
+  end
+  
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,8 @@
 class Event < ApplicationRecord
+  belongs_to :creator, class_name: "User"
+  
+  validates :name, presence: true
+  validates :date, presence: true
+  validates :location, presence: true
+  validates :description, presence: true, length: { minimum: 1 }
 end
